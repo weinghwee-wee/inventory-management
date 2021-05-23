de4e6c410ac22bf0243d4c8c9ff96e8f4349604c
weinghwee <weinghwee.wee@hotmail.com>
2021-05-23 21:31:48 +0800

    added success flag in api response

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -3,8 +3,32 @@ import axios from "axios";
 /**
  * @deprecated
  */
+
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3000",
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => {
+    res.data.success = true
+    return res
+  },
+  async (err) => {
+    if (err.response) {
+      err.response.data.success = false
+      return err.response
+    }
+
+    // show error modal (Network error etc)
+    // store.dispatch(showModalAction("Error", err.message, null, "Close"));
+
+    return {
+      data: {
+        message: err.message,
+        success: false
+      },
+    };
+  }
+);
 
 export default axiosInstance;
